167b089151eb2e53a8cf1d6ccd53430ca3ef60e8
Thiago Curvelo <tcurvelo@gmail.com>
2013-10-17 16:40:50 -0300

    Update script for ubuntu-13.10

--- ubuntu-VM-postinstall.sh
+++ ubuntu-VM-postinstall.sh
@@ -5,9 +5,33 @@
 ## de desenvolvimento rodando Ubuntu    ##
 ##########################################
 
-sudo echo "$USER    ALL=(ALL)   NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/$USER > /dev/null
-sudo chmod 0400 /etc/sudoers.d/$USER
+function instala_pacotes {
+    for pacote in $*
+    do
+        sudo apt-get -qq install $pacote
+    done
+}
 
+function remove_pacotes {
+    for pacote in $*
+    do
+        sudo apt-get -qq remove $pacote
+    done
+}
+
+
+## Inclui usuario no sudoers, sem senha
+########################################################################
+SUDOER_LINE="$USER ALL=(ALL) NOPASSWD:ALL"
+sudo grep "$SUDOER_LINE" /etc/sudoers.d/$USER > /dev/null 2> /dev/null
+if [ $? -ne 0 ]
+then
+    sudo echo $SUDOER_LINE | sudo tee -a /etc/sudoers.d/$USER > /dev/null
+    sudo chmod 0400 /etc/sudoers.d/$USER
+fi
+
+## Configura o proxy
+########################################################################
 AUTH_INPUT=$(zenity --username --password)
 
 if [ "$AUTH_INPUT" != "" ]
@@ -28,7 +52,7 @@ then
     }
 EOF
 
-    sudo cat >> ~/.profile << EOF 
+    sudo cat >> ~/.profile << EOF
 export ftp_proxy="$PROXY_URL";
 export http_proxy="$PROXY_URL";
 export https_proxy="$PROXY_URL";
@@ -49,12 +73,31 @@ EOF
 
 fi
 
-#Apaga alguns pacotes não-essenciais
-sudo apt-get -y remove rhythmbox brasero totem thunderbird bluez 
-sudo apt-get -y remove gwibber-service-twitter gwibber-service-facebook \
-account-plugin-twitter account-plugin-facebook 
-sudo apt-get -y remove unity-lens-music unity-lens-photos \
-unity-scope-home unity-lens-video
+
+## Apaga alguns pacotes não-essenciais
+########################################################################
+remove_pacotes \
+    account-plugin-facebook \
+    account-plugin-twitter \
+    bluez\
+    brasero \
+    gwibber-service-facebook \
+    gwibber-service-twitter \
+    rhythmbox \
+    thunderbird \
+    totem \
+    ;
+
+remove_pacotes \
+    unity-lens-music \
+    unity-lens-photos \
+    unity-lens-shopping \
+    unity-lens-video \
+    ;
+
+
+## Inclui alguns repositorios
+########################################################################
 
 #Repositorio para o Sublime Text 2
 sudo -E add-apt-repository -y ppa:webupd8team/sublime-text-2
@@ -63,40 +106,98 @@ sudo -E add-apt-repository -y ppa:webupd8team/sublime-text-2
 sudo -E add-apt-repository -y ppa:tualatrix/ppa
 
 #Repositorio para faenza-icon-theme
-sudo -E add-apt-repository -y ppa:tiheum/equinox
+#sudo -E add-apt-repository -y ppa:tiheum/equinox
 
 #Repositorio para python2.6
-sudo -E add-apt-repository -y ppa:fkrull/deadsnakes
-
-sudo apt-get -y update
-sudo apt-get -y install alien build-essential checkinstall curl faenza-icon-theme git gnome-tweak-tool \
-guake libjpeg-dev libsasl2-dev libxml2-dev libxslt1-dev mercurial python2.6-dev python-dev python-pip \
-python-setuptools python-virtualenv rubygems sublime-text-2-beta subversion synapse ubuntu-tweak 
-unixodbc-dev vim wireshark xclip zlib1g-dev zsh 
+#sudo -E add-apt-repository -y ppa:fkrull/deadsnakes
 
 
+## Instala pacotes para desenvolvimento
+########################################################################
+
+sudo apt-get -qq update
+instala_pacotes \
+    alien \
+    build-essential \
+    checkinstall \
+    curl \
+    git \
+    libjpeg-dev \
+    libsasl2-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    mercurial \
+    python-dev \
+    python-pip \
+    python-setuptools \
+    python-virtualenv \
+    rubygems \
+    sublime-text \
+    subversion \
+    unixodbc-dev \
+    vim \
+    wireshark \
+    xclip \
+    zlib1g-dev \
+    zsh \
+    ;
+#    python2.6-dev \
+
+#Instala oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | bash
-
-#Instala o suporte a pt_BR
-sudo apt-get -y install language-pack-pt language-pack-gnome-pt hunspell-en-ca hyphen-en-us \
-libreoffice-help-en-gb libreoffice-help-pt libreoffice-help-pt-br \
-libreoffice-l10n-en-gb libreoffice-l10n-en-za libreoffice-l10n-pt libreoffice-l10n-pt-br \
-myspell-en-au myspell-en-gb myspell-en-za myspell-pt mythes-en-au mythes-en-us \
-openoffice.org-hyphenation wbrazilian wbritish wportuguese
-
-#Instala pacotes adicionais do VirtualBox
-sudo apt-get -y install virtualbox-guest-additions virtualbox-guest-x11
-
-#Limpa cache do apt
-sudo apt-get clean
-
 chsh -s /usr/bin/zsh
 
-#Ajustes de Desktop
+
+## Instala pacotes para desktop ubuntu
+########################################################################
+
+instala_pacotes \
+    synapse \
+    ubuntu-tweak \
+    gnome-tweak-tool \
+    ;
+#    faenza-icon-theme \
+
+
+## Instala o suporte a pt_BR
+########################################################################
+instala_pacotes \
+    hunspell-en-ca \
+    hyphen-en-us \
+    language-pack-gnome-pt \
+    language-pack-pt \
+    libreoffice-help-en-gb \
+    libreoffice-help-pt \
+    libreoffice-help-pt-br \
+    libreoffice-l10n-en-gb \
+    libreoffice-l10n-en-za \
+    libreoffice-l10n-pt \
+    libreoffice-l10n-pt-br \
+    myspell-en-au \
+    myspell-en-gb \
+    myspell-en-za \
+    myspell-pt \
+    mythes-en-au \
+    mythes-en-us \
+    openoffice.org-hyphenation \
+    wbrazilian \
+    wbritish \
+    wportuguese \
+    ;
+
+## Instala pacotes adicionais do VirtualBox
+########################################################################
+instala_pacotes \
+    virtualbox-guest-additions-iso \
+    virtualbox-guest-x11 \
+    ;
+
+## Ajustes no Desktop
+########################################################################
 dconf write /com/canonical/indicator/datetime/show-date true
 dconf write /com/canonical/indicator/datetime/show-day true
 dconf write /com/canonical/unity/lenses/remote-content-search '"none"'
-dconf write /org/gnome/desktop/interface/icon-theme '"Faenza-Ambiance"'
+#dconf write /org/gnome/desktop/interface/icon-theme '"Faenza-Ambiance"'
 dconf write /com/canonical/unity/launcher/favorites '["application://nautilus.desktop", "application://chromium-browser.desktop", "application://sublime-text-2.desktop", "application://firefox.desktop", "unity://running-apps", "unity://expo-icon", "unity://devices"]'
 
 sudo mkdir -p /var/cache/buildout/eggs
@@ -104,3 +205,5 @@ sudo mkdir -p /var/cache/buildout/dlcache
 sudo -E chown -R $USER /var/cache/buildout/ -R
 
 
+#Limpa cache do apt
+sudo apt-get clean
