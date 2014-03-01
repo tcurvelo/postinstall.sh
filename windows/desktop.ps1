4a1de1ddaa7e21a856c32d3ca6269777c250a669
Thiago Curvelo <tcurvelo@gmail.com>
2014-03-01 14:16:54 -0800

    remove "superhidden" config

--- windows/desktop.ps1
+++ windows/desktop.ps1
@@ -48,5 +48,4 @@ iex "./citrix.exe"
 $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
 Set-ItemProperty $key Hidden 1
 Set-ItemProperty $key HideFileExt 0
-Set-ItemProperty $key ShowSuperHidden 1
 Stop-Process -processname explorer
