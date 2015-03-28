4669d2a50a1534dc7a26d1421c82ed44e30a3b69
Thiago Curvelo <tcurvelo@gmail.com>
2015-03-28 14:45:47 -0300

    Add gvim icon to panel

--- linux/ubuntu-desktop.sh
+++ linux/ubuntu-desktop.sh
@@ -87,7 +87,7 @@ INSTALL_PKGS \
 dconf write /com/canonical/indicator/datetime/show-date true
 dconf write /com/canonical/indicator/datetime/show-day true
 dconf write /com/canonical/unity/lenses/remote-content-search '"none"'
-dconf write /com/canonical/unity/launcher/favorites '["application://chromium-browser.desktop", "application://firefox.desktop", "application://terminator.desktop", "application://sublime-text.desktop", "application://nautilus.desktop", "unity://running-apps", "unity://expo-icon", "unity://devices"]'
+dconf write /com/canonical/unity/launcher/favorites '["application://chromium-browser.desktop", "application://firefox.desktop", "application://terminator.desktop", "application://gvim.desktop", "application://nautilus.desktop", "unity://running-apps", "unity://expo-icon", "unity://devices"]'
 
 # Cleanup
 sudo apt-get clean
