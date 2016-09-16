d1e628be374f11624e7037fae2b7dfcd46cf3a1d
Thiago Curvelo <tcurvelo@gmail.com>
2016-09-16 19:32:46 -0300

    Add caps/escape remap into the dconf-tweak script

--- linux/scripts/dconf-tweaks.sh
+++ linux/scripts/dconf-tweaks.sh
@@ -2,5 +2,6 @@
 ########################################################################
 dconf write /com/canonical/indicator/datetime/show-date true
 dconf write /com/canonical/indicator/datetime/show-day true
-dconf write /com/canonical/unity/lenses/remote-content-search '"none"'
 dconf write /com/canonical/unity/launcher/favorites '["application://chromium-browser.desktop", "application://firefox.desktop", "application://terminator.desktop", "application://gvim.desktop", "application://nautilus.desktop", "unity://running-apps", "unity://expo-icon", "unity://devices"]'
+dconf write /com/canonical/unity/lenses/remote-content-search '"none"'
+dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
