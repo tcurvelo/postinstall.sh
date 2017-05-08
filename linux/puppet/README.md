baed2932c9a946339987681cec159422046467a4
Thiago Curvelo <tcurvelo@gmail.com>
2017-05-08 19:31:16 +0000

    Fix pyenv installation through puppet

---
+++ linux/puppet/README.md
@@ -0,0 +1,14 @@
+
+Usage:
+
+    sudo apt install puppet-common
+    sudo puppet module install saz-locales --version 2.3.0
+    sudo puppet apply default.pp --modulepath modules:~/.puppet/etc/code/modules/
+
+TODO:
+
+- Install node && npm
+- Install global npm pkgs: grunt-cli, jsctags, jshint
+- Install plone dependencies
+- Configure buildout dirs
+- Install docker
--- linux/puppet/modules/lab/manifests/pyenv.pp
+++ linux/puppet/modules/lab/manifests/pyenv.pp
@@ -1,5 +1,35 @@
 class lab::pyenv($pyenv_root) {
 
+  $python2 = "2.7.13"
+  $python3 = "3.6.1"
+  $python2_major = "python2.7"
+  $python3_major = "python3.6"
+  $pyenv_path = [
+    "${pyenv_root}/bin",
+    "${pyenv_root}/plugins/python-build/bin/",
+    "/bin",
+    "/usr/bin"
+  ]
+  $pyenv_env = ["PYENV_ROOT=${pyenv_root}"]
+
+  package {
+      [
+        "libbz2-dev",
+        "libncurses5-dev",
+        "libncursesw5-dev",
+        "libreadline-dev",
+        "libsqlite3-dev",
+        "libssl-dev",
+        "llvm",
+        "make",
+        "tk-dev",
+        "wget",
+        "xz-utils",
+        "zlib1g-dev",
+      ]:
+      ensure => installed,
+  }
+
   exec { "pyenv-installer":
       command => "/usr/bin/git clone https://github.com/pyenv/pyenv.git ${pyenv_root}",
       creates => "${pyenv_root}"
@@ -11,28 +41,73 @@ class lab::pyenv($pyenv_root) {
     require => Exec["pyenv-installer"]
   }
 
-  exec { "python2":
-    command => "${pyenv_root}/bin/pyenv install 2.7.13",
-    path => ["${pyenv_root}/bin", "${pyenv_root}/plugins/python-build/bin/", "/bin", "/usr/bin"],
-    timeout => 0,
-    environment => ["PYENV_ROOT=${pyenv_root}"],
-    creates => "${pyenv_root}/versions/2.7.13",
-    require => Exec["pyenv-init"]
-  }
-
-  exec { "python3":
-    command => "${pyenv_root}/bin/pyenv install 3.6.1",
-    path => ["${pyenv_root}/bin", "${pyenv_root}/plugins/python-build/bin/", "/bin", "/usr/bin"],
-    timeout => 0,
-    environment => ["PYENV_ROOT=${pyenv_root}"],
-    creates => "${pyenv_root}/versions/3.6.1",
-    require => Exec["pyenv-init"]
-  }
 
   exec { "pyenv-virtualenv":
     command => "/usr/bin/git clone https://github.com/pyenv/pyenv-virtualenv.git ${pyenv_root}/plugins/pyenv-virtualenv",
     creates => "${pyenv_root}/plugins/pyenv-virtualenv",
     require => Exec["pyenv-installer"]
+  }
+
+  exec { "python2":
+    command => "${pyenv_root}/bin/pyenv install ${python2}",
+    path => $pyenv_path,
+    timeout => 0,
+    environment => $pyenv_env,
+    creates => "${pyenv_root}/versions/${python2}",
+    require => Exec["pyenv-init"]
+  }
+
+  exec { "tools2-init":
+    command => "${pyenv_root}/bin/pyenv virtualenv ${python2} tools2",
+    path => $pyenv_path,
+    timeout => 0,
+    environment => $pyenv_env,
+    creates => "${pyenv_root}/versions/tools2",
+    require => Exec["python2", "pyenv-virtualenv"]
+  }
+
+  exec { "tools2-pkgs":
+    command => "${pyenv_root}/versions/${python2}/tools2/bin/pip install neovim",
+    path => $pyenv_path,
+    timeout => 0,
+    environment => $pyenv_env,
+    creates => "${pyenv_root}/versions/tools2/lib/${python2_major}/site-packages/neovim",
+    require => Exec["tools2-init"]
+  }
+
+  exec { "python3":
+    command => "${pyenv_root}/bin/pyenv install ${python3}",
+    path => $pyenv_path,
+    timeout => 0,
+    environment => $pyenv_env,
+    creates => "${pyenv_root}/versions/${python3}",
+    require => Exec["pyenv-init"]
+  }
+
+  exec { "tools3-init":
+    command => "${pyenv_root}/bin/pyenv virtualenv ${python3} tools3",
+    path => $pyenv_path,
+    timeout => 0,
+    environment => $pyenv_env,
+    creates => "${pyenv_root}/versions/tools3",
+    require => Exec["python3", "pyenv-virtualenv"]
+  }
+
+  exec { "tools3-pkgs":
+    command => "${pyenv_root}/versions/${python3}/tools3/bin/pip install neovim",
+    path => $pyenv_path,
+    timeout => 0,
+    environment => $pyenv_env,
+    creates => "${pyenv_root}/versions/tools3/lib/${python3_major}/site-packages/neovim",
+    require => Exec["tools3-init"]
+  }
+
+  exec { "pyenv-globals":
+    command => "${pyenv_root}/bin/pyenv global ${python3} ${python2} tools3 tools2",
+    path => $pyenv_path,
+    timeout => 0,
+    environment => $pyenv_env,
+    require => Exec["tools2-init", "tools3-init"]
   }
 
 }
