b8da3201d6aa3631a5f24f2731fc608a3e51364f
Thiago Curvelo <tcurvelo@gmail.com>
2017-07-16 18:24:50 -0300

    Add a simple postinstall.sh for mac osx

---
+++ osx/postinstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+brew install \
+      cask \
+      ctags \
+      neovim \
+      readline \
+      ripgrep \
+      sqlite \
+      tree \
+      ;
+
+brew cask install \
+      alfred \
+      battle-net \
+      docker \
+      firefox \
+      flux \
+      google-chrome \
+      iterm2 \
+      seashore \
+      spotify \
+      steam \
+      visual-studio-code \
+      ;
