72b2cf649116e579ff7552bed245f45d2a2964c6
Thiago Curvelo <tcurvelo@gmail.com>
2017-10-23 20:30:44 -0300

    Remove unused scripts

--- linux/wip/ubuntu-desktop-vbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Instala pacotes adicionais do VirtualBox
-########################################################################
-
-sudo apt-get -q -y install  \
-    virtualbox-guest-additions-iso \
-    virtualbox-guest-x11 \
-    ;
