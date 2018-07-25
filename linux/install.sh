349d510948cca26de61dfb3469e792cb1362f439
Thiago Curvelo <tcurvelo@gmail.com>
2018-07-24 22:09:47 -0300

    Install vscode

--- linux/install.sh
+++ linux/install.sh
@@ -3,6 +3,7 @@ source common.sh
 source packages.sh
 source pyenv.sh
 source docker.sh
+source vscode.sh
 
 ENABLE_SUDO
 UPDATE_LOCALES
@@ -13,12 +14,12 @@ PYENV_INSTALL ${packages['pip']}
 NPM_INSTALL ${packages['npm']}
 
 case $1 in
-  desktop-minimal)
-    APT_INSTALL ${packages['desktop-common']} ${packages['desktop-minimal']}
+  desktop-*)
+    APT_INSTALL ${packages['desktop-common']}
     INSTALL_DOCKER
-    ;;
-  desktop-complete)
-    APT_INSTALL ${packages['desktop-common']} ${packages['desktop-complete']}
-    INSTALL_DOCKER
-    ;;
+    INSTALL_CODE
+    case $1 in
+      desktop-minimal) APT_INSTALL ${packages['desktop-minimal']};;
+      desktop-complete) APT_INSTALL ${packages['desktop-complete']};;
+    esac
 esac
