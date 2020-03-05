6844670f74f44e7fd98f21a61a94d48d75b21a05
Thiago Curvelo <tcurvelo@gmail.com>
2020-03-05 20:51:57 -0300

    Check if sudo is present

--- linux/common.sh
+++ linux/common.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+which sudo > /dev/null && SUDO=sudo
+
 ## Common functions
 ########################################################################
 function APT_UPDATE {
@@ -7,37 +9,38 @@ function APT_UPDATE {
     last_repo_added=$(stat -c %Y /etc/apt/sources.list* | sort -n -r | head -1)
     now=$(date +%s)
     if [ $last_update -lt $last_repo_added ] || [ $(($now-$last_update)) -gt 3600 ] ; then
-        sudo apt-get -y update
+        $SUDO apt-get -y update
     fi
 }
 
 function APT_INSTALL {
   APT_UPDATE
-  sudo apt-get -y --ignore-missing install $(echo $*)
+  $SUDO apt-get -y --ignore-missing install $(echo $*)
 }
 
 function APT_REMOVE {
-    sudo apt-get -y remove $(echo $*)
+    $SUDO apt-get -y remove $(echo $*)
 }
 
 function NPM_INSTALL {
-    sudo npm install -g npm $(echo $*)
+    $SUDO npm install -g npm $(echo $*)
 }
 
 function UPDATE_LOCALES() {
   APT_INSTALL locales \
-    && sudo locale-gen --purge \
+    && $SUDO locale-gen --purge \
        en_US en_US.utf8 \
        pt_BR pt_BR.utf8 \
-    && sudo dpkg-reconfigure --unseen locales
+    && $SUDO dpkg-reconfigure --unseen locales
 }
 
 function ENABLE_SUDO() {
+  [ "$SUDO" == "" ] && return 1
   SUDOER_LINE="$USER ALL=(ALL) NOPASSWD:ALL"
-  sudo grep "$SUDOER_LINE" /etc/sudoers.d/$USER > /dev/null 2> /dev/null
+  $SUDO grep "$SUDOER_LINE" /etc/sudoers.d/$USER > /dev/null 2> /dev/null
   if [ $? -ne 0 ]; then
-      echo $SUDOER_LINE | sudo tee -a /etc/sudoers.d/$USER > /dev/null
-      sudo chmod 0400 /etc/sudoers.d/$USER
+      echo $SUDOER_LINE | $SUDO tee -a /etc/sudoers.d/$USER > /dev/null
+      $SUDO chmod 0400 /etc/sudoers.d/$USER
   fi
 }
 
