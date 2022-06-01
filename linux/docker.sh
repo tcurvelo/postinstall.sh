4c5167c60fd9248afa8cfbf6c47e9de13a6e894e
Thiago Curvelo <tcurvelo@gmail.com>
2022-05-31 21:57:20 -0300

    update docker source

--- linux/docker.sh
+++ linux/docker.sh
@@ -12,10 +12,9 @@ function INSTALL_DOCKER {
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
   # Set up the stable repository
-  sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   # Install docker ce
   APT_UPDATE
