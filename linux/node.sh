389021160cd42279270e1b568018f54be4a39acd
Thiago Curvelo <tcurvelo@gmail.com>
2022-05-31 21:57:32 -0300

    update node source

--- linux/node.sh
+++ linux/node.sh
@@ -1,6 +1,6 @@
 
 function INSTALL_NODE  {
-    curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
     sudo apt-get install -y nodejs
 }
 
